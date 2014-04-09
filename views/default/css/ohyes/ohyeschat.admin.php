e2681720f0beb545a347289030ab8012159def55
Arsalan Shah <arsalan@buddyexpress.net>
2014-04-10 00:34:07 +0500

    css

---
+++ views/default/css/ohyes/ohyeschat.admin.php
@@ -0,0 +1,165 @@
+/*
+* Copyright iNFORMATIKON TECHNOLOGIES 2014
+*/
+body {
+	background:#EDEDED;
+	margin:0 auto;
+	left:0;
+	right:0;
+	top:0;
+	font-family: sans-serif;
+}
+fieldset {
+	border:0;
+}
+.ohyeschat-admin-header {
+	background:#38809C;
+    height:70px;
+}
+.ohyeschat-admin-menu-top { 
+   background-color: #F5F5F5;
+   -webkit-box-shadow: 0 4px 10px -4px rgba(0, 0, 0, 0.05);
+   box-shadow: 0 4px 10px -4px rgba(0, 0, 0, 0.05);
+   height:50px;
+}
+.ohyeschat-admin-menu-top .items {
+   width:930px;
+   margin:0px auto;
+}
+.ohyeschat-admin-menu-top li {
+	 display: inline-table;
+}
+.ohyeschat-admin-menu-top li a{
+ display: inline-table;
+ padding: 16px;
+ font-weight: bold;
+ color: #949494;
+ cursor:pointer;
+ text-decoration:none;
+}
+.ohyeschat-admin-menu-top li a:hover{
+ color:#BBB;
+}
+.ohyeschat-admin-menu-top li:hover{
+ color: #BEBEBE;
+}
+.ohyeschat-admin-header .left {
+ color: #fff;
+ font-size: 29px;
+ margin-left: 20px;
+ padding: 18px;
+}
+.ohyeschat-admin-container {
+ background: #FFF;
+ width: 930px;
+ margin: 0 auto;
+ margin-top: 20px;
+ min-height: 300px;
+ border: 1px solid #DFDDDD;
+ border-radius: 3px;
+ margin-bottom:20px;
+ padding: 20px;
+}
+h2 {
+ color: #38809C;
+ font-size: 18px;	
+}
+.button-home {
+ background: #38809C;
+ height: 20px;
+ padding: 15px;
+ color: #FFF;
+ width: 200px;
+ text-align: center;
+}
+.module {
+ width: 270px;
+ display: inline-table;
+ margin-right:20px;
+ min-height: 200px;
+ border: 1px solid #CCC; 	
+}
+.module .head{
+ background: #38809C;
+ color: #FFF;
+ padding: 20px;
+ text-align: center;
+ font-weight: bold;
+ font-size: 18px;
+}
+#module-green {
+	background:#AECE4E;
+}
+#module-red {
+	background:#E77755;
+}
+.button-yellow {
+ background: #F4C84F;
+ padding: 20px;
+ text-align: center;
+ font-weight: bold;
+ color: #FFF;
+}
+.chat-system-message {
+background: #59ACC0;
+border: 1px solid #3590AD;
+padding: 20px;
+color: #FFF;
+font-weight: bold;	
+}
+.system-message-close a{
+float: right;
+margin-top: -30px;
+color: #FFF;
+text-decoration: none;
+}
+.ohyeschat-admin-container input {
+ padding: 14px;
+border: 1px solid #EEE;	
+}
+.track-user-msg {
+	text-align:center;
+}
+.ohyes-chat-expand {
+width: 660px;
+margin: 0 auto;
+overflow: auto;
+height: 600px;
+}
+.message-item-expand {
+border-bottom:1px solid rgba(0, 0, 0, 0.29);
+padding: 3px;	
+margin-bottom: 16px;
+-moz-box-shadow: 0 0 10px #EEE;
+-webkit-box-shadow: 0 0 10px #EEE;
+box-shadow: 0 0 10px #EEE;
+}
+.message-item-expand .icon{
+	display:inline-table;
+}
+.message-item-expand .message{
+	display:inline-table;
+	width: 570px;
+    margin-top: -21px;
+    margin-left: 5px;
+} 
+.footer {
+	width: 920px;
+	margin:0 auto;
+}
+.footer .data-inner {
+	font-size: 11px;
+}
+.footer .data-inner a{
+color: #000;
+font-weight: bold;
+text-decoration: none;
+}
+
+.button-yellow a{
+	color:#fff;
+	text-decoration:none;
+}
+.messages-item img{
+	border-radius:20px;
+}
