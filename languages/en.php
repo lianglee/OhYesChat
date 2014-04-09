10092597300b4276adde441ec7f231cdd4bb420e
Arsalan Shah <arsalan@buddyexpress.net>
2014-04-10 00:32:55 +0500

    en.php

--- languages/en.php
+++ languages/en.php
@@ -10,12 +10,28 @@
  */ 
  
 $english = array(	
+	'ohyeschat' => 'OhYesChat',			 
 	'ohyes:chat:online' => 'Online',
 	'ohyes:chat' => 'Chat',
 	'ohyes:chat:total' => 'Chat (%s)',
 	'ohyes:chat:notification' => 'Notifications',
 	'ohyes:chat:no:online' => 'No friend is online',
 	'ohyes:chat:no:message' => 'There is currently no message',
+	'track:user:mesg' => 'Type usernames of user in below fields',
+	'track:user:chat:hist' => "Chat History Between %s and %s",
+	'track:user' => 'OhYesChat TrackUser',
+	'ohyes:chat' => 'Home',
+	'ohyes:track:messages' => 'Track Messages',
+	'ohyes:back:admin' => 'Back to Admin Panel',
+	'ohyes:bug:report' => 'Bug Report',
+	'ohyes:authors' => 'Authors',
+	'ohyeschat:new:version:avaialbe' => "A new version OhYesChat %s is available",
+	'ohyeschat:new:version:latest' => 'You are using latest version of OhYesChat %s',
+	'ohyeschat:chat:welcome' => 'Welcome to OhYesChat',
+	'ohyeschat:new:version:error' => 'Can not connect to server',
+	'module:title:chat:update' => 'OhYesChat Update',
+	'module:title:chat:latest:messages' => 'Latest Messages',
+	'module:title:chat:total:messages' => 'Total Messages',
 
 
  );
