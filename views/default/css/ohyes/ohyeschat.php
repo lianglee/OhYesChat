304443255cc6f6a8111f5f4d35e4aa69dc3de685
Arsalan Shah <arsalan@buddyexpress.net>
2014-04-10 00:34:10 +0500

    css

--- views/default/css/ohyes/ohyeschat.php
+++ views/default/css/ohyes/ohyeschat.php
@@ -66,6 +66,7 @@
    -webkit-box-shadow: inset 0 1px rgba(255, 255, 255, 0.5);
    border: 1px solid #BAC0CD;
    height: 29px;
+   border-radius:4px;
 }
 .OhYesChat .ChatBar .inner:hover {
    background: #FDFEFF;
@@ -94,6 +95,7 @@
    -webkit-box-shadow: inset 0 1px rgba(255, 255, 255, 0.5);
    border: 1px solid #BAC0CD;
    height: 29px;
+   border-radius: 4px;
 }
 .OhYesChat .ChatTab .inner .box {
    margin-top: -7px;
@@ -104,20 +106,20 @@
  border-radius: 0px;
  background: none repeat scroll 0% 0% #FFF;
  border: medium none;
- width: 247px;
+ width: 225px;
  height: 28px;
- z-index: 1;
- border-top: 1px solid #CCC;
+ margin-top: 1px;
  position: absolute;
 }
 .OhYesChat .ChatTab .inner:hover {
    background: #FDFEFF;
 }
 .OhYesChat .data {
-  overflow: auto;
+  overflow: -moz-scrollbars-vertical; 
+  overflow-y: scroll;
   height: 245px;
-  border-left: 1px solid #BAC0CD;
-  border-right: 1px solid #BAC0CD;
+  border-left: 2px solid #BAC0CD;
+  border-right: 2px solid #BAC0CD;
 }
 .ohyeschat-new-message {
   float: right;
@@ -146,6 +148,8 @@
   margin-left: 5px;
   position: fixed;
   height: 268px;
+  border-top-left-radius: 4px;
+  border-top-right-radius: 4px;
 }
 .friends-list .data {
  overflow: auto;
@@ -165,6 +169,7 @@ display: inline-table;
 }
 .friends-list-item:hover{
 	background:#eee;
+    height:29px;
 }
 .friends-list-item .name{ 
  margin-top: -25px;
@@ -177,24 +182,77 @@ display: inline-table;
 }
 
 .Tab-Title {
-  background: #fff;
-  width: 248px;
-  min-height: 268px;
-  margin-top: -268px;
+  background: #F2F3F5;
+  width: 250px;
+  min-height: 278px;
+  margin-top: -278px;
   margin-left: 5px;
   position: fixed;
   height: 268px;
-  border-left: 1px solid #BAC0CD;border-right: 1px solid #BAC0CD;
+  border-top-left-radius: 4px;
+  border-top-right-radius: 4px;
 }
-.Tab-Title .message-item {
+.Tab-Title .message-item .ohyeschat-message-text{
+ margin-top: -38px;
+ margin-left: 51px;
+ width: 165px;
+ background-image: -webkit-linear-gradient(bottom, #F2F2F2, #FFF);
+ background-color: #F7F7F7; 
+ border: 1px solid #DFDFDF;
+ border: 1px solid rgba(0, 0, 0, 0.18);
+ border-bottom-color: rgba(0, 0, 0, 0.29);
+ -webkit-border-radius: 4px;
+ -webkit-box-shadow: 0 1px 0 #DCE0E6;
+ padding:3px;
+}
+.Tab-Title .message-item .icon{
+ padding:5px;
+ height:40px;
+ width:40px;
+}
+.ohyeschat-triangle {
+ border-top: 5px solid rgba(0, 0, 0, 0);
+ border-bottom: 5px solid rgba(0, 0, 0, 0);
+ margin-left: 46px;
+ margin-top: -32px;
 
+}
+.ohyeschat-triangle-white { 
+ float: left;
+ border-right: 5px solid #B8B8B8;
+}	
+.ohyeschat-triangle-blue {
+ float:right;
+ border-left: 5px solid #AFD0FE;
+}
+.Tab-Title .message-item .ohyeschat-message-text-sender{
+ background-color: #DBEDFE;
+ background-image: url("<?php echo elgg_get_site_url();?>mod/OhYesChat/images/message-bg-sender.png") repeat-x;
+ background-image: -webkit-gradient(linear, center bottom, center top, from(#C7DEFE), to(#E7F1FE));
+ background-image: -webkit-linear-gradient(bottom, #C7DEFE, #E7F1FE);
+ 
+ border: 1px solid #DFDFDF;
+ border: 1px solid rgba(0, 0, 0, 0.18);
+ border-bottom-color: rgba(0, 0, 0, 0.29);
+ 
+ -webkit-border-radius: 4px;
+ -webkit-box-shadow: 0 1px 0 #DCE0E6;
+ 
+ padding:3px;
+ margin-top: -38px;
+ margin-left: 51px;
+ width: 165px;
 }
 
 .OhYesChat-Titles {
-	background:#6D84B4;
-	height:23px;
-	border:1px solid #3C5998;
+ background: #4C66A4;
+ padding: 5px;
+ background-image: url("<?php echo elgg_get_site_url();?>mod/OhYesChat/images/chat-title-bg.png");
+ border: 1px solid #3C5998;
+ border-top-left-radius: 4px;
+ border-top-right-radius: 4px;
 }
+
 .OhYesChat-Inline-Table {
   display: inline-table;	
 }
@@ -206,8 +264,14 @@ display: inline-table;
   font-size: 12px;
   cursor:pointer;
 }
+.OhYesChat-Titles .options .item:hover{
+ background: #5E72A2;
+ width: 17px;
+ margin-right: -4px;
+ text-align: center;
+}
 .OhYesChat-Titles:hover {
-	background:#8395BB;
+	background:#4C66A4;
 }
 
 .OhYesChat-Titles .text {
@@ -217,19 +281,50 @@ display: inline-table;
   padding: 2px;
 }
 .notification-window {
-   margin-top: -324px;
+   margin-top: -332px;
    margin-left: -196px;
    border: 1px solid #CCC;
    background: #F2F3F5;
+   border-top-left-radius: 4px;
+   border-top-right-radius: 4px;
 }
 .notification-window .data {
  overflow: overlay;
  height: 245px;
 }
-.friends-list-item:hover{
-	background:#eee;
+.ohyeschat-message-sending {
+ background: #FFF; 
+ border: medium none;
+ width: 225px;
+ height: 28px;
+ margin-top: 1px;
+ padding: 10px;
+ display:none;
+ position: absolute;
 }
-
+.ohyes-chat-expand {
+width: 660px;
+margin: 0 auto;
+overflow: auto;
+height: 600px;
+}
+.message-item-expand {
+border-bottom:1px solid rgba(0, 0, 0, 0.29);
+padding: 3px;	
+margin-bottom: 16px;
+-moz-box-shadow: 0 0 10px #EEE;
+-webkit-box-shadow: 0 0 10px #EEE;
+box-shadow: 0 0 10px #EEE;
+}
+.message-item-expand .icon{
+	display:inline-table;
+}
+.message-item-expand .message{
+	display:inline-table;
+	width: 570px;
+    margin-top: -21px;
+    margin-left: 5px;
+}
 /****************************
  OhYesChat Icons
 *****************************/
@@ -249,7 +344,7 @@ display: inline-table;
   height:13px;
 }
 .ohyes-chat-loading {
-  padding: 105px;
+  padding: 100px;
 }
 .OhYesChat-Icon-Loading {
   background:url("<?php echo elgg_get_site_url();?>mod/OhYesChat/images/loading.gif") no-repeat;
@@ -262,3 +357,29 @@ display: inline-table;
   width:16px;
   height:16px;
 }
+.OhYesChat-Icon-Smilies {
+  background:url("<?php echo elgg_get_site_url();?>mod/OhYesChat/images/smilies.png") no-repeat;
+  width:14px;
+  height:14px;
+}
+.OhYesChat-Icon-Smilies:hover {
+ opacity:0.7;
+}
+.OhYesChat-Icon-LoadingSmall {
+  background:url("<?php echo elgg_get_site_url();?>mod/OhYesChat/images/loading-small.gif") no-repeat;
+  width:16px;
+  height:11px;
+}
+.OhYesChat-Icon-Expend {
+  background:url("<?php echo elgg_get_site_url();?>mod/OhYesChat/images/popup.png") no-repeat;
+  width:10px;
+  height:11px;
+  margin-right:8px;
+}
+.OhYesChat-Icon-Expend:hover{
+  opacity:0.9;
+}
+
+
+
+
